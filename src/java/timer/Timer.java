ab668efc9ebad60f56797a9bbfd5ae5e3174e923
VeeAyeInIn <40581838+VeeAyeInIn@users.noreply.github.com>
2018-09-04 10:48:09 -0400

    Changed Asynchronous Behavior

--- src/java/timer/Timer.java
+++ src/java/timer/Timer.java
@@ -7,52 +7,53 @@ import java.util.concurrent.TimeUnit;
 
 public class Timer extends Thread {
 
-	private final ThreadFunction function;
-	private final long millis;
-	private final long loops;
+    private final Thread thread = new Thread(this);
+    private final ThreadFunction function;
+    private final long millis;
+    private final long loops;
 
-	public Timer(ThreadFunction function, long millis) {
-		this.function = function;
-		this.millis = millis;
-		this.loops = 1;
-	}
+    public Timer(ThreadFunction function, long millis) {
+        this.function = function;
+        this.millis = millis;
+        this.loops = 1;
+    }
 
-	public Timer(ThreadFunction function, long millis, long loops) {
-		this.function = function;
-		this.millis = millis;
-		this.loops = loops;
-	}
+    public Timer(ThreadFunction function, long millis, long loops) {
+        this.function = function;
+        this.millis = millis;
+        this.loops = loops;
+    }
 
-	public Timer(ThreadFunction function, TimeUnit unit, long duration) {
-		this.function = function;
-		this.millis = unit.toMillis(duration);
-		this.loops = 1;
-	}
+    public Timer(ThreadFunction function, TimeUnit unit, long duration) {
+        this.function = function;
+        this.millis = unit.toMillis(duration);
+        this.loops = 1;
+    }
 
-	public Timer(ThreadFunction function, TimeUnit unit, long duration, long loops) {
-		this.function = function;
-		this.millis = unit.toMillis(duration);
-		this.loops = loops;
-	}
+    public Timer(ThreadFunction function, TimeUnit unit, long duration, long loops) {
+        this.function = function;
+        this.millis = unit.toMillis(duration);
+        this.loops = loops;
+    }
 
-	private void loop(Thread thread, long millis, long loops) throws InterruptedException {
+    private void loop(Thread thread, long millis, long loops) throws InterruptedException {
 
-		Validate.notNull(thread, "Thread cannot be null");
-		Validate.isTrue(millis > 0, "Millis cannot be less than or equal to 0");
-		Validate.isTrue(loops > 0, "Loops cannot be less than or equal to 0");
+        Validate.notNull(thread, "Thread cannot be null");
+        Validate.isTrue(millis > 0, "Millis cannot be less than or equal to 0");
+        Validate.isTrue(loops > 0, "Loops cannot be less than or equal to 0");
 
-		while (loops > 0) {
-			sleep(millis);
-			function.run(this);
-			loops--;
-		}
-	}
+        while (loops > 0) {
+            sleep(millis);
+            function.execute(thread);
+            loops--;
+        }
+    }
 
-	public void run() {
-		try {
-			loop(this, millis, loops);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+    public void run() {
+        try {
+            loop(thread, millis, loops);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
